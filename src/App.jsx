c66e0b512ce6498a722c112a2111ff42b9f312ab
JunSeok Lee <galaxysayegi@gmail.com>
2024-05-14 09:21:44 +0900

    컴포넌트는 각자 자기의 상태만 갖는다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,24 @@
 import React, { useState } from "react";
 
-function App(props) {
-  const [person, setPerson] = useState({
-    name: "",
-    address: "",
-    city: "",
-    email: "",
-  });
+function MyComp() {
+  const [count, setCount] = useState(0);
 
   return (
     <div>
+      <div>{count}</div>
       <div>
-        <input
-          type="text"
-          placeholder={"이름"}
-          onChange={(e) => {
-            const { ...nextPerson } = person;
-            nextPerson.name = e.target.value;
-            setPerson(nextPerson);
-          }}
-        />
+        <button onClick={() => setCount(count + 1)}>UP</button>
       </div>
-      <div>
-        <input
-          type="text"
-          placeholder={"이메일"}
-          onChange={(e) => {
-            const { ...nextPerson } = person;
-            nextPerson.email = e.target.value;
-            setPerson(nextPerson);
-          }}
-        />
-      </div>
-      <div>
-        <input
-          type="text"
-          placeholder={"도시"}
-          onChange={(e) => {
-            const nextPerson = { ...person, city: e.target.value };
-            setPerson(nextPerson);
-          }}
-        />
-      </div>
-      <div>
-        <input
-          type="text"
-          placeholder={"주소"}
-          onChange={(e) => {
-            setPerson({ ...person, address: e.target.value });
-          }}
-        />
-      </div>
-      <ul>
-        <li>name: {person.name}</li>
-        <li>email: {person.email}</li>
-        <li>city: {person.city}</li>
-        <li>address: {person.address}</li>
-      </ul>
+    </div>
+  );
+}
+
+function App(props) {
+  return (
+    <div>
+      <MyComp />
+      <hr />
+      <MyComp />
     </div>
   );
 }
