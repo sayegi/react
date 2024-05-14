3a8e6c7700d852a60305d3c5451d0a32fd623658
JunSeok Lee <galaxysayegi@gmail.com>
2024-05-14 10:24:29 +0900

    "lift state up"으로 컴포넌트의 상태 유지 가능

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,25 @@
 import React, { useState } from "react";
 
-function MyComp({ color }) {
-  const [count, setCount] = useState(0);
-
+function MyBox({ count, setCount }) {
   return (
-    <div style={{ border: `5px solid ${color}` }}>
-      <div>{count}</div>
-      <div>
-        <button onClick={() => setCount(count + 1)}>UP</button>
-      </div>
+    <div>
+      <button onClick={() => setCount(count + 1)}>UP</button>
+      <p>{count}</p>
     </div>
   );
 }
 
 function App(props) {
-  const [view1, setView1] = useState(true);
-  const [view2, setView2] = useState(true);
+  const [display, setDisplay] = useState(true);
+  const [count, setCount] = useState(0);
   return (
     <div>
       <input
-        type={"checkbox"}
-        checked={view1}
-        onClick={(e) => setView1(e.target.checked)}
+        type="checkbox"
+        checked={display}
+        onChange={(e) => setDisplay(e.target.checked)}
       />
-      {view1 && <MyComp color={"blue"} />}
-      <hr />
-      <input
-        type={"checkbox"}
-        checked={view2}
-        onClick={(e) => setView2(e.target.checked)}
-      />
-      {view2 && <MyComp color={"red"} />}
+      {display && <MyBox count={count} setCount={setCount} />}
     </div>
   );
 }
