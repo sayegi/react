87195edb188271c7ea02f5a08ab2f4f4d1781a21
JunSeok Lee <galaxysayegi@gmail.com>
2024-05-14 11:35:16 +0900

    context 사용해서 데이터 전달

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { createContext, useContext, useState } from "react";
 
+// step1 : create the context
+const MessageContext = createContext("");
+
+function MyBox() {
+  // step2 : use the context
+  const message = useContext(MessageContext);
+
+  return <div>{message}</div>;
+}
+
+function MySection() {
+  return <MyBox />;
+}
+
+function MyContainer() {
+  return <MySection />;
+}
+
+// context
+// https://react.dev/learn/passing-data-deeply-with-context
 function App(props) {
-  return <div></div>;
+  const [message, setMessage] = useState("");
+
+  return (
+    <div>
+      <input type="text" onChange={(e) => setMessage(e.target.value)} />
+      <p>{message}</p>
+      <hr />
+      {/* step3 : provide the context */}
+      <MessageContext.Provider value={message}>
+        <MyContainer />
+      </MessageContext.Provider>
+    </div>
+  );
 }
 
 export default App;
